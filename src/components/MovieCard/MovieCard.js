67e2ccde0439e272cb1a0cd3d66cbe6e4862234e
Hoa Luu <hoaluu@hoas-mbp-2.myfiosgateway.com>
2021-05-18 23:18:36 -0400

    successful api requestfor popular

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -6,6 +6,12 @@ class MovieCard extends Component {
         this.state = {}
     }
 
+    componentDidMount(){
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=dd073e56f8e18f35e55f427502bbfabc')
+            .then(res => res.json())
+            .then(res => console.log(res))
+    }
+
     render(){
         return(
             <h1>
